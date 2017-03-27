a371e97c381348a6995a27fb2acfc6365967567b
Tom Glanzman <dragon@slac.stanford.edu>
2017-03-27 13:27:03 -0700

    Added new feature to use pre-generated instanceCatalogs.  Change is backward compatible

--- NERSC/config.sh
+++ NERSC/config.sh
@@ -4,6 +4,12 @@
 ###  Hanma's visit DB of obsHistIDs and lists of sensors
 export DC1_VISIT_DB='/global/common/cori/contrib/lsst/production/DC1/visitDBs/2017-01-29_chipPerVisitData_newAfterburnerOutput_fID1447_RandomDitherFieldPerVisit_randomRotDithered_nonDiscRegion_131052TotChipsToSimulate.pickle'
 
+##
+### Instance Catalog
+##
+export PHOSIM_IC_GEN='STATIC'
+
+## Dynamic Instance Catalogs...
 ### OpSim DB for generating instanceCatalog
 export DC1_OPSIM_DB='/global/common/cori/contrib/lsst/production/DC1/opsimDBs/minion_1016_sqlite_new_dithers.db'
 export DC1_OPSIM_DB='/global/cscratch1/sd/desc/opsimDBs/minion_1016_sqlite_new_dithers.db'
@@ -11,10 +17,12 @@ export DC1_OPSIM_DB='/global/cscratch1/sd/desc/opsimDBs/minion_1016_sqlite_new_d
 ## minimum magnitude (max brightness) for objects in sky catalog
 export DC1_MINMAG=10.0
 
-## Location of phoSim inputs
-DC1base=`dirname ${DC1_ROOT}`
-export PHOSIM_CATALOGS=${DC1base}/catalogs
+## Static Instance Catalogs...
+## 3/27/2017 - Mustafa's private scratch area for collecting instanceCatalogs
+export PHOSIM_CATALOGS="/global/cscratch1/sd/mustafa/DC1-phoSim-3"
 
+
+DC1base=`dirname ${DC1_ROOT}`
 #export PHOSIM_SEDS=${DC1base}/SEDs
 #export PHOSIM_SEDS='/global/common/cori/contrib/lsst/lsstDM/w.2016.20/lsstsw/stack/Linux64/sims_sed_library/12.0'
 #export PHOSIM_SEDS='/global/projecta/projectdirs/lsst/production/DC1/phoSim-data/SEDs'
@@ -27,9 +35,6 @@ export PHOSIM_SCR_ROOT=/global/cscratch1/sd/desc/Pipeline-tasks/${TASKNAME}
 #export PHOSIM_SCR_ROOT=${SCRATCH}/Pipeline-tasks/${TASKNAME}
 ## Flag for cleaning up $SCRATCH after visit is complete (0=disable, 1=enable)
 export PHOSIM_SCR_CLEANUP=0
-
-## List of all phoSim catalogs for this task
-export PHOSIMICL=${DC1_CONFIGDIR}/catalog.list
 
 ## Enable or disable the E2ADC step (0=disable, 1=enable)
 export PHOSIM_E2ADC=0
